1fc8b59780988beb8c45782def894f5e9311beed
Toluwa Nafiu <nafiutc@rose-hulman.edu>
2018-10-09 21:52:59 -0400

    Finished Exam 2: Problem 2

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 Exam 2, problem 2.
 
 Authors: Dave Fisher, David Mutchler, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Toluwa Nafiu.  October 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import math
 import time
@@ -109,8 +109,8 @@ def main():
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
 
-    # run_test_problem2a()
-    # run_test_problem2b()
+    run_test_problem2a()
+    run_test_problem2b()
 
 
 def run_test_problem2a():
@@ -196,9 +196,14 @@ def problem2a(triangle):
       :rtype: Triangle
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    side_len_a = 2*triangle.a
+    side_len_b = 2*triangle.b
+    side_len_c = 2*triangle.c
+    new_triangle = Triangle(side_len_a, side_len_b, side_len_c)
+    return new_triangle
 
 
 def run_test_problem2b():
@@ -354,10 +359,14 @@ def problem2b(triangles):
       :rtype: int | float
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # IMPORTANT: See the HINT just before the DEF of this function.
     # -------------------------------------------------------------------------
+    area_sum = 0
+    for k in range(len(triangles)):
+        area_sum = area_sum + triangles[k].get_area()
+    return area_sum
 
 
 ###############################################################################
